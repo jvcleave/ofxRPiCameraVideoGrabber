82f27e1db0f9268ba38d36deb196b220f25319b3
Jason Van Cleave <jvcleave@gmail.com>
2014-03-23 05:18:16 +0000

    apps compiling in examples and apps folder

--- example-remote/addons/ofxHTTP/src/ofxHTTP.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "ofx/HTTP/Server/BasicServer.h"
-//#include "ofx/HTTP/IPVideo/BasicIPVideoServer.h"
-
--- example-texture-mode/src/main.cpp
+++ example-texture-mode/src/main.cpp
@@ -1,23 +1,6 @@
 #include "ofMain.h"
 #include "ofGLProgrammableRenderer.h"
 
-//#define use_texturedRemoteApp
-//#define use_textureApp
-//#define use_nonTextureApp
-#define use_shaderApp
-
-#ifdef use_texturedRemoteApp
-#include "texturedRemoteApp.h"
-int main()
-{
-	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1280, 720, OF_WINDOW);
-	ofRunApp( new texturedRemoteApp()); 
-}
-#endif
-
-#ifdef use_textureApp
 #include "textureApp.h"
 int main()
 {
@@ -26,26 +9,3 @@ int main()
 	ofSetupOpenGL(1280, 720, OF_WINDOW);
 	ofRunApp( new textureApp());
 }
-#endif
-
-#ifdef use_nonTextureApp
-#include "nonTextureApp.h"
-int main()
-{
-	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1280, 720, OF_WINDOW);
-	ofRunApp( new nonTextureApp());
-}
-#endif
-
-#ifdef use_shaderApp
-#include "shaderApp.h"
-int main()
-{
-	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1280, 720, OF_WINDOW);
-	ofRunApp( new shaderApp());
-}
-#endif
