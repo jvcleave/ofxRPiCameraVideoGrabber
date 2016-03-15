208febc5e9518ed73de237700f3f67a4d3f34197
jason van cleave <jvcleave@gmail.com>
2016-03-15 04:37:12 +0000

    missing files?

---
+++ src/DirectEngine.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "BaseEngine.h"
+#include "DirectDisplay.h"
+
+
+
+class DirectEngine: public BaseEngine
+{
+public:
+	DirectEngine();
+	~DirectEngine();
+	void setup(OMXCameraSettings);
+    int getFrameCounter();
+    DirectDisplay displayManager;
+private:
+    
+    OMX_ERRORTYPE setupDisplay(int x, int y, int width, int height);
+    int frameCounter;
+    
+	OMX_ERRORTYPE onCameraEventParamOrConfigChanged();
+	
+	static OMX_ERRORTYPE
+    cameraEventHandlerCallback(OMX_HANDLETYPE,
+                               OMX_PTR,
+                               OMX_EVENTTYPE,
+                               OMX_U32,
+                               OMX_U32,
+                               OMX_PTR);
+	static OMX_ERRORTYPE
+    encoderFillBufferDone(OMX_IN OMX_HANDLETYPE,
+                          OMX_IN OMX_PTR,
+                          OMX_IN OMX_BUFFERHEADERTYPE*);
+
+	
+    
+
+};
