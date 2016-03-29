35f040d6df1848e24e526d26bd4fe3737daabccc
jason van cleave <jvcleave@gmail.com>
2016-03-29 18:01:24 +0000

    allow OMX addons to compile together

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+ADDON_NAME = ofxRPiCameraVideoGrabber
+ADDON_DESCRIPTION =  Control the Raspberry Pi Module with OpenMax
+ADDON_AUTHOR = Jason Van Cleave
+ADDON_TAGS = "raspberry pi, camera module"
+ADDON_URL = https://github.com/jvcleave/ofxRPiCameraVideoGrabber
+
+
+common:
+ADDON_CFLAGS = -Wno-deprecated-declarations -Wno-sign-compare -Wno-unknown-pragmas -Wno-unused-function -Wno-unused-but-set-variable
