74e45a726ee30e771f49c9fd0090e8bbbeb3dfcf
Jason Van Cleave <jvcleave@gmail.com>
2018-10-26 02:59:42 -0400

    comment

--- src/OMXCameraSettings.h
+++ src/OMXCameraSettings.h
@@ -207,7 +207,16 @@ public:
         
         return result;
     }
+/*
+    https://www.raspberrypi.org/blog/new-camera-mode-released/
     
+    2592×1944 1-15fps, video or stills mode, Full sensor full FOV, default stills capture
+    1920×1080 1-30fps, video mode, 1080p30 cropped
+    1296×972 1-42fps, video mode, 4:3 aspect binned full FOV. Used for stills preview in raspistill.
+    1296×730 1-49fps, video mode, 16:9 aspect , binned, full FOV (width), used for 720p
+    640×480 42.1-60fps, video mode, up to VGAp60 binned
+    640×480 60.1-90fps, video mode, up to VGAp90 binned
+ */
     enum RPI_CAMERA_PRESET 
     {
         PRESET_FASTEST_480P,
